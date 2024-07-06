ee19e5171cb611c6f64ab1617b59b479bf22e26d
sironAzevedo <sirondba@gmail.com>
2024-07-06 16:22:23 -0300

    lib demo calculadora

--- src/main/java/com/br/azevedo/Main.java
+++ src/main/java/com/br/azevedo/Main.java
@@ -1,7 +1,10 @@
 package com.br.azevedo;
 
+import com.br.azevedo.demo.Calculadora;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Calculadora calculadora = new Calculadora();
+        System.out.println(calculadora.soma(1, 1));
     }
 }
--- src/main/java/com/br/azevedo/demo/Calculadora.java
+++ src/main/java/com/br/azevedo/demo/Calculadora.java
@@ -1,4 +1,14 @@
 package com.br.azevedo.demo;
 
 public class Calculadora {
+
+    public Calculadora() {}
+
+    public Integer soma(Integer a, Integer b) {
+        return a + b;
+    }
+
+    public Integer subtracao(Integer a, Integer b) {
+        return a - b;
+    }
 }
