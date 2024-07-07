2a7f43395bc86b82b3a6356b891878e70907439e
sironAzevedo <sirondba@gmail.com>
2024-07-07 09:19:33 -0300

    removendo pacote demo

--- src/main/java/com/br/azevedo/Main.java
+++ src/main/java/com/br/azevedo/Main.java
@@ -1,10 +1,6 @@
 package com.br.azevedo;
 
-import com.br.azevedo.demo.Calculadora;
-
 public class Main {
     public static void main(String[] args) {
-        Calculadora calculadora = new Calculadora();
-        System.out.println(calculadora.soma(1, 1));
     }
 }
